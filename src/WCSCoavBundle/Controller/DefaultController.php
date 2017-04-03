bca792a6a994c511766495f89307fad351c045b2
Tandav81 <davidtanguy@gmail.com>
2017-04-03 17:36:28 +0200

    crud

---
+++ src/WCSCoavBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WCSCoavBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('WCSCoavBundle:Default:index.html.twig');
+    }
+}
